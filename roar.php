c22226ed7ca30a62201d12ac4872e1540b726ae0
meisenfn9 <meisen@oasgames.com>
2019-08-29 09:48:31 +0800

    *

--- roar.php
+++ roar.php
@@ -11,29 +11,44 @@ namespace Dino\Play;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Yaml\Dumper;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 date_default_timezone_set('Asia/Shanghai');
+$stime = microtime(true);
 
-$container = new ContainerBuilder();
+if (!file_exists(__DIR__ . '/cached_container.php')) {
+    $container = new ContainerBuilder();
+    $container->setParameter('root_dir', __DIR__);
 
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
-$loader->load('services.yml');
+    $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
+    $loader->load('services.yml');
+
+    $container->compile();
+
+    $dumper = new PhpDumper($container);
+    file_put_contents(__DIR__ . '/cached_container.php', $dumper->dump());
+}
+
+require_once __DIR__ . '/cached_container.php';
+$container = new \ProjectServiceContainer();
 
 
-$handlerDefinition = new Definition(StreamHandler::class);
-$handlerDefinition->setArguments([__DIR__ . '/dino.log']);
-$container->setDefinition('logger.stream_handler', $handlerDefinition);
+//$handlerDefinition = new Definition(StreamHandler::class);
+//$handlerDefinition->setArguments([__DIR__ . '/dino.log']);
+//$container->setDefinition('logger.stream_handler', $handlerDefinition);
 
 
-$stdOutLoggerDefinition = new Definition('Monolog\Handler\StreamHandler');
-$stdOutLoggerDefinition->setArguments(['php://stdout']);
-$container->setDefinition('logger.std_out_handler', $stdOutLoggerDefinition);
+//$stdOutLoggerDefinition = new Definition('Monolog\Handler\StreamHandler');
+//$stdOutLoggerDefinition->setArguments(['php://stdout']);
+//$container->setDefinition('logger.std_out_handler', $stdOutLoggerDefinition);
 
 
 //$loggerDefinition = new Definition(Logger::class);
@@ -49,7 +64,9 @@ $container->setDefinition('logger.std_out_handler', $stdOutLoggerDefinition);
 
 
 runApp($container);
-function runApp(ContainerBuilder $container)
+$etime = microtime(true);
+var_dump(($etime-$stime)*1000);
+function runApp(ContainerInterface $container)
 {
     $container->get('logger')->info(date('Ymd His'));
 }
